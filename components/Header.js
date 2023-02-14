c88e90c4b1eec054f7e1d32d5404b1a40146f32b
Arjun Singh <singharjun.biz@gmail.com>
2023-02-14 20:51:29 +0530

    Add Header component

---
+++ components/Header.js
@@ -0,0 +1,6 @@
+// Internal dependencies
+import * as styles from "./Header.module.scss";
+
+export default function Header() {
+  return <header className={styles.header}>This is the header</header>;
+}
