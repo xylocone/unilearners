fe5438f44703722dec92da346c7a9e8bb6653a34
Arjun Singh <singharjun.biz@gmail.com>
2023-02-15 01:44:47 +0530

    Create Home view

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,41 @@
+import Image from "next/image";
+
+// Internal dependencies
+import Header from "../components/Header";
+import Button from "../components/Button";
+
+import styles from "./Home.module.scss";
+
 export default function Home() {
-  return <p>This is the Home Page</p>;
+  return (
+    <>
+      <Header />
+      <Homepage />
+    </>
+  );
+}
+
+function Homepage() {
+  return (
+    <div className={styles.home}>
+      <div className={styles.content}>
+        <h1 className={styles.heading}>UniLearners</h1>
+        <h3 className={styles.subheading}>
+          Your child&apos;s future is our responsibility.
+        </h3>
+        <div className={styles.buttons}>
+          <Button>Hire a Tutor</Button>
+          <span>or</span>
+          <Button>Become a Tutor</Button>
+        </div>
+      </div>
+      <Image
+        className={styles.image}
+        src="/home.png"
+        alt="Illustration"
+        width={100}
+        height={100}
+      />
+    </div>
+  );
 }
