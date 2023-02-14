c8a8312c23a51e1ccf884c5f458e7e94852ece9e
Arjun Singh <singharjun.biz@gmail.com>
2023-02-14 21:53:08 +0530

    Update Header styles

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,26 @@
+import { AiOutlineMenu } from "react-icons/ai";
+
 // Internal dependencies
 import * as styles from "./Header.module.scss";
 
 export default function Header() {
-  return <header className={styles.header}>This is the header</header>;
+  return (
+    <header className={styles.header}>
+      <Logo />
+      <MenuButton />
+    </header>
+  );
+}
+
+function MenuButton() {
+  return (
+    <button className={styles.menu}>
+      <AiOutlineMenu />
+      <span>Menu</span>
+    </button>
+  );
+}
+
+function Logo() {
+  return <p className={styles.logo}>Uni</p>;
 }
